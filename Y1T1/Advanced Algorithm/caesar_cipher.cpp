0f3b047300dcf30ed92ca41249c1a33311bf5cd6
KKC73 <50138914+KKC73@users.noreply.github.com>
2022-12-30 10:51:39 +0700

    Improve No finished

---
+++ Y1T1/Advanced Algorithm/caesar_cipher.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+string encrypt(string text, int s = 3){
+    string result = "";
+
+    for (int i = 0; i < text.length(); i++) {
+        if (isupper(text[i]))
+            result += char(int(text[i] + s - 65) % 26 + 65);
+        else
+            result += char(int(text[i] + s - 97) % 26 + 97);
+    }
+    return result;
+}
+
+string decrypt(string text, int s = 3){
+    s = 26 - s;
+    string result = "";
+
+    for (int i = 0; i < text.length(); i++) {
+        if (isupper(text[i]))
+            result += char(int(text[i] + s - 65) % 26 + 65);
+        else
+            result += char(int(text[i] + s - 97) % 26 + 97);
+    }
+    return result;
+}
